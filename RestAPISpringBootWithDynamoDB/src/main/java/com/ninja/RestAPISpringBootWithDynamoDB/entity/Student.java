91f2686f7503820a62c0b7ed087b95d305c8c34c
yuvraj.sharma <ys8484529@gmail.com>
2024-01-18 20:58:58 +0530

    Initial commit

--- RestAPISpringBootWithDynamoDB/src/main/java/com/ninja/RestAPISpringBootWithDynamoDB/entity/Student.java
+++ RestAPISpringBootWithDynamoDB/src/main/java/com/ninja/RestAPISpringBootWithDynamoDB/entity/Student.java
@@ -4,11 +4,14 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTypeConverted;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @DynamoDBTable(tableName="Student")
@@ -25,4 +28,8 @@ public class Student {
 	
 	@DynamoDBAttribute
 	private String grade;
+
+	@DynamoDBAttribute(attributeName = "report")
+	@DynamoDBTypeConverted(converter = ReportTranslator.class)
+	private Report report;
 }
